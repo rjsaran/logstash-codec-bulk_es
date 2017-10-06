c2d0f9c0ee494bbb9dd5ff14091b3d7aa8f75612
Ramjeet Saran <ramjeet.saran@paytm.com>
2017-10-06 16:04:55 +0530

    initial commit

---
+++ DEVELOPER.md
@@ -0,0 +1,2 @@
+# logstash-codec-bulk_es
+Example codec plugin. This should help bootstrap your effort to write your own codec plugin!
---
+++ lib/logstash/codecs/bulk_es.rb
@@ -0,0 +1,52 @@
+# encoding: utf-8
+require "logstash/codecs/base"
+require "logstash/codecs/line"
+require "logstash/json"
+require "logstash/namespace"
+
+
+# Todo add description
+class LogStash::Codecs::BulkEs < LogStash::Codecs::Base
+
+  # The codec name
+  config_name "bulk_es"
+
+  def register
+    @lines = LogStash::Codecs::Line.new
+    @lines.charset = "UTF-8"
+    @state = :start
+    @metadata = Hash.new
+  end # def register
+
+  def decode(data)
+    @lines.decode(data) do |bulk_message|
+      begin
+        line = LogStash::Json.load(bulk_message.get("message"))
+        case @state
+        when :metadata
+          event = LogStash::Event.new(line)
+          event.set("@metadata", @metadata)
+          yield event
+          @state = :start
+        when :start
+          @metadata = line[line.keys[0]]
+          @metadata["action"] = line.keys[0].to_s
+          @state = :metadata
+          if line.keys[0] == 'delete'
+            event = LogStash::Event.new()
+            event.set("@metadata", @metadata)
+            yield event
+            @state = :start
+          end
+        end
+      rescue LogStash::Json::ParserError => e
+        @logger.error("JSON parse failure. Bulk ES messages must in be UTF-8 JSON", :error => e, :data => data)
+      end
+    end
+  end # def decode
+
+  def encode(data)
+    raise "Not implemented"
+  end # def encode
+
+end # class LogStash::Codecs::BulkEs
